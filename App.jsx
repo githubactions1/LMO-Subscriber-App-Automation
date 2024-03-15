d82800f9c706040e01ee809d9c117b7bc5e17cf6
githubactions1 <pinnellianilsai123@gmail.com>
2024-03-15 15:31:51 +0530

    status

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import HomeScreen from './src/HomeScreen';
+import LoginScreen from './src/LoginScreen';
+import ResetScreen from './src/ResetScreen';
+import SplashScreen from './src/SplashScreen';
+import RenewList from './src/RenewList';
+import ComplaintScreen from './src/ComplaintScreen';
+import ProfileScreen from './src/ProfileScreen';
+import HelpAndSupportScreen from './src/HelpAndSupportScreen';
+import InvoiceScreen from './src/InvoiceScreen';
+import VoipUsage from './src/VoipUsage';
+import ChangePasswordScreen from './src/ChangePasswordScreen';
+import ChangePhoneNoScreen from './src/ChangePhoneNoScreen';
+import AboutUsScreen from './src/AboutUsScreen';
+import BuyChannelScreen from './src/BuyChannelScreen';
+import HsiUsage from './src/HsiUsage';
+import NotificationsScreen from './src/NotificationsScreen';
+
+const App = () => {
+  return (
+    <>
+      {/* <SplashScreen /> */}
+      {/* <LoginScreen /> */}
+      {/* <ResetScreen /> */}
+      <HomeScreen />
+      {/* <RenewList /> */}
+      {/* <ComplaintScreen /> */}
+      {/* <ProfileScreen /> */}
+      {/* <HelpAndSupportScreen /> */}
+      {/* <InvoiceScreen /> */}
+      {/* <VoipUsage /> */}
+      {/* <ChangePasswordScreen /> */}
+      {/* <ChangePhoneNoScreen /> */}
+      {/* <AboutUsScreen /> */}
+      {/* <BuyChannelScreen /> */}
+      {/* <HsiUsage /> */}
+      {/* <NotificationsScreen /> */}
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
